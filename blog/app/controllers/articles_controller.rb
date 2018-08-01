f6d0ea77bf149a848599c705aa3ae3cfe136148a
marc-bouvier <m.bouvier.dev@gmail.com>
2018-08-01 08:47:15 +0200

    refactoring articles_controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,23 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
@@ -14,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-   
+
     if @article.update(article_params)
       redirect_to @article
     else
@@ -25,20 +38,8 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-   
+
     redirect_to articles_path
-  end
-  
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def edit
-    @article =  Article.find(params[:id])
   end
 
   private
